05d8ed48fc74214528ec9381f7d44a412753e8ce
Beerelly Prudhvi Maharishi <prudhvimaharishi@gmail.com>
2020-10-20 19:34:23 +0530

    initial commit

---
+++ src/main/java/com/prometheus/rknaturals/RkNaturalsApplication.java
@@ -0,0 +1,13 @@
+package com.prometheus.rknaturals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkNaturalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkNaturalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prometheus/rknaturals/RkNaturalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometheus.rknaturals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkNaturalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
